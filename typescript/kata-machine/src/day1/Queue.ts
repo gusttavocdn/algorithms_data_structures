671e8b92238bbeb8f1ef58ebf41b39bd45bc3357
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-07-16 01:07:35 -0300

    feat: make Queque

--- typescript/kata-machine/src/day1/Queue.ts
+++ typescript/kata-machine/src/day1/Queue.ts
@@ -1,18 +1,53 @@
+class QNode<T> {
+	public data: T;
+	public next: QNode<T> | null;
+
+	constructor(item: T) {
+		this.data = item;
+		this.next = null;
+	}
+}
+
 export default class Queue<T> {
-    public length: number;
+	public length: number;
+	public head: QNode<T> | null;
+	public tail: QNode<T> | null;
 
-    
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
 
-    constructor() {
-    }
+	enqueue(item: T): void {
+		const newNode = new QNode(item);
 
-    enqueue(item: T): void {
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+			this.length++;
+			return;
+		}
 
-}
-    deque(): T | undefined {
+		this.tail!.next = newNode;
+		this.tail = newNode;
+		this.length++;
+	}
 
-}
-    peek(): T | undefined {
+	deque(): T | undefined {
+		if (!this.head) return undefined;
 
-}
+		const removedNode = this.head;
+
+		this.head = this.head!.next;
+		this.length--;
+
+		if (this.length === 0) this.tail = null;
+		return removedNode?.data;
+	}
+
+	peek(): T | undefined {
+		if (!this.head) return undefined;
+		return this.head.data;
+	}
 }
